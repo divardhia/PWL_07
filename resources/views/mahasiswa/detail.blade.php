8ce9a311a72f4fb8472b94815579cb1ec7a91407
divardhia <divardhiass@gmail.com>
2021-03-26 22:35:17 +0700

    edit detail.blade.php

--- resources/views/mahasiswa/detail.blade.php
+++ resources/views/mahasiswa/detail.blade.php
@@ -14,7 +14,9 @@
                         <li class="list-group-item"><b>Nama: </b>{{ $Mahasiswa->Nama }}</li>
                         <li class="list-group-item"><b>Kelas: </b>{{ $Mahasiswa->Kelas }}</li>
                         <li class="list-group-item"><b>Jurusan: </b>{{ $Mahasiswa->Jurusan }}</li>
+                        <li class="list-group-item"><b>Tanggal Lahir: </b>{{ $Mahasiswa->tanggalLahir }}</li>
                         <li class="list-group-item"><b>No_Handphone: </b>{{ $Mahasiswa->No_Handphone }}</li>
+                        <li class="list-group-item"><b>Email: </b>{{ $Mahasiswa->email }}</li>
                     </ul>
                 </div>
                 <a class="btn btn-success mt-3" href="{{ route('mahasiswa.index') }}">Kembali</a>
