0d6a98ff22b1468b0d0969200bf9e4b29e9fd0ae
divardhia <divardhiass@gmail.com>
2021-03-26 22:44:06 +0700

    edit paginate dalam MahasiswaController

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -14,10 +14,8 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-         //fungsi eloquent menampilkan data menggunakan pagination
-         $mahasiswa = Mahasiswa::all(); // Mengambil semua isi tabel
-         $posts = Mahasiswa::orderBy('Nim', 'desc')->paginate(6);
-         return view('mahasiswa.index', compact('mahasiswa'))->with('i', (request()->input('page', 1) - 1) * 5); 
+        $mahasiswa = Mahasiswa::paginate(5);
+        return view('mahasiswa.index', ['mahasiswa' => $mahasiswa]);
     }
 
     /**
